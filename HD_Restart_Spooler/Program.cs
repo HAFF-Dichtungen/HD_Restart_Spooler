1e453945b363749af17495541cb366c6619fef66
HD_EU <82515556+HAFF-Dichtungen@users.noreply.github.com>
2022-01-27 15:16:22 +0100

    Projektdateien hinzufügen.

---
+++ HD_Restart_Spooler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.ServiceProcess;
+
+void startup()
+{
+
+    Console.WriteLine("Stoppe Spooler");
+    //Spooler stoppen
+    ServiceController service = new("Spooler");
+    if ((!service.Status.Equals(ServiceControllerStatus.Stopped)) &&
+      (!service.Status.Equals(ServiceControllerStatus.StopPending)))
+    {
+
+        service.Stop();
+        service.WaitForStatus(ServiceControllerStatus.Stopped);
+    }
+
+    Console.WriteLine("Starte Spooler");
+    //Spooler starten
+    service.Start();
+    service.WaitForStatus(ServiceControllerStatus.Running);
+
+
+}
+
+
+Console.WriteLine("Spooler Neustarten");
+startup();
+
+
